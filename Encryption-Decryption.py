60f7cd4fa1c12a77065da803157e9aaee8bc8ddb
Raviteja Ainampudi <raviteja.ainam@gmail.com>
2020-03-27 11:09:18 -0400

    Updating Project to Python3

--- Encryption-Decryption.py
+++ Encryption-Decryption.py
@@ -7,126 +7,132 @@ Created on Wed Aug 03 15:34:06 2016
 from Crypto.Hash import MD5
 from Crypto.Cipher import AES
 import os, random, sys
- 
-def encrypt(key, filename):
+
+
+class DynamicEncryptionAndDecryption(object):
+    def __init__(self, filename=None):
+        self.filename = filename
+
+    def encrypt(self, key, filename):
         chunksize = 128 * 1024
-        outFile = os.path.join(os.path.dirname(filename), "(Secured)"+os.path.basename(filename))
+        outFile = os.path.join(os.path.dirname(filename), "(Secured)" + os.path.basename(filename))
         filesize = str(os.path.getsize(filename)).zfill(16)
         IV = ''
- 
+
         for i in range(16):
-                IV +=  chr(random.randint(0, 0xFF))
-       
+            IV += chr(random.randint(0, 0xFF))
         encryptor = AES.new(key, AES.MODE_CBC, IV)
- 
+
         with open(filename, "rb") as infile:
-                with open(outFile, "wb") as outfile:
-                        outfile.write(filesize)
-                        outfile.write(IV)
-                        while True:
-                                chunk = infile.read(chunksize)
-                               
-                                if len(chunk) == 0:
-                                        break
- 
-                                elif len(chunk) % 16 !=0:
-                                        chunk += ' ' *  (16 - (len(chunk) % 16))
- 
-                                outfile.write(encryptor.encrypt(chunk))
- 
- 
-def decrypt(key, filename):
+            with open(outFile, "wb") as outfile:
+                outfile.write(filesize)
+                outfile.write(IV)
+                while True:
+                    chunk = infile.read(chunksize)
+
+                    if len(chunk) == 0:
+                        break
+
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' ' * (16 - (len(chunk) % 16))
+
+                    outfile.write(encryptor.encrypt(chunk))
+
+    def decrypt(self, key, filename):
         outFile = os.path.join(os.path.dirname(filename), os.path.basename(filename[9:]))
         chunksize = 128 * 1024
         with open(filename, "rb") as infile:
-                filesize = infile.read(16)
-                IV = infile.read(16)
- 
-                decryptor = AES.new(key, AES.MODE_CBC, IV)
-               
-                with open(outFile, "wb") as outfile:
-                        while True:
-                                chunk = infile.read(chunksize)
-                                if len(chunk) == 0:
-                                        break
- 
-                                outfile.write(decryptor.decrypt(chunk))
- 
-                        outfile.truncate(int(filesize))
-       
-def allfiles():
+            filesize = infile.read(16)
+            IV = infile.read(16)
+
+            decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+            with open(outFile, "wb") as outfile:
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+
+                    outfile.write(decryptor.decrypt(chunk))
+
+                outfile.truncate(int(filesize))
+
+    @staticmethod
+    def allfiles():
         allFiles = []
         for root, subfiles, files in os.walk(os.getcwd()):
-                for names in files:
-                        allFiles.append(os.path.join(root, names))
- 
+            for names in files:
+                allFiles.append(os.path.join(root, names))
         return allFiles
- 
-print ""      
-choice = raw_input("Do you want to L - List the Files, E - Encrypt or D - Decrypt? ==")
-print ""
-password = raw_input("Please enter the `Password/Key` to be used: ")
- 
-encFiles = allfiles()
- 
-if choice == "E":
-        print ""
-        subchoice = raw_input("Want to encrypt all the Files ? Y- Yes or N - No ? ")
+
+
+def choices():
+    ed_object = DynamicEncryptionAndDecryption()
+    choice = input("Do you want to L - List the Files, E - Encrypt or D - Decrypt? ==")
+    print("\n")
+    perform_multiple_encryption = input(f"Do you want to perform multi-layered encryption? Y-Yes or N-No: ")
+    if perform_multiple_encryption.lower() not in ("yes", "y"):
+        perform_multiple_encryption = False
+        password = input("Please enter the `Password/Key` to be used: ")
+    else:
+
+    encFiles = ed_object.allfiles()
+
+    if choice == "E":
+        print("")
+        subchoice = input("Want to encrypt all the Files ? Y- Yes or N - No ? :")
         if subchoice == "Y":
-          for Tfiles in encFiles:
+            for Tfiles in encFiles:
                 if os.path.basename(Tfiles).startswith("(Secured)"):
-                        print "%s is already encrypted" %str(Tfiles)
-                        pass
- 
+                    print(f"{Tfiles} is already encrypted")
+                    pass
                 elif Tfiles == os.path.join(os.getcwd(), sys.argv[0]):
-                        pass
+                    pass
                 else:
-                        encrypt(MD5.new(password).digest(), str(Tfiles))
-                        print "Done Encryption %s" %str(Tfiles)
-                        os.remove(Tfiles)
- 
+                    ed_object.encrypt(MD5.new(password).digest(), str(Tfiles))
+                    print(f"Done Encryption for {Tfiles}")
+                    os.remove(Tfiles)
         elif subchoice == "N":
-            print ""
-            filename = raw_input("Enter the Filename to Encrypt: ")
+            print("")
+            filename = input("Enter the Filename to Encrypt: ")
             if not os.path.exists(filename):
-                print "Given file does not exist"
+                print(f"Given file {filename} does not exist")
                 sys.exit(0)
             elif filename.startswith("(Secured)"):
-                print "%s was already encrypted" %filename
+                print(f"File {filename} was already encrypted")
                 sys.exit()
             else:
-                encrypt(MD5.new(password).digest(), filename)
-                print "Done Encryption %s" %filename
+                ed_object.encrypt(MD5.new(password).digest(), filename)
+                print(f"Done Encryption of {filename}")
                 os.remove(filename)
-            
         else:
-            print ""
-            print "Enter either Y or N" 
- 
-elif choice == "D":
-          print ""
-               
-          filename = raw_input("Enter the filename to decrypt: ")
-          if not os.path.exists(filename):
-                print "Given file does not exist"
-                sys.exit(0)
-          elif not filename.startswith("(Secured)"):
-                print "%s is was never encrypted" %filename
-                sys.exit()
-          else:
-                decrypt(MD5.new(password).digest(), filename)
-                print "Done Decryption %s" %filename
-                os.remove(filename)
+            print("\n Enter either Y or N")
 
-elif choice == "L":
-        print ""
-        print "The files present in the current directory are:"
+    elif choice == "D":
+        print("")
+        filename = input("Enter the filename to decrypt: ")
+        if not os.path.exists(filename):
+            print(f"Given file {filename} does not exist")
+            sys.exit(0)
+        elif not filename.startswith("(Secured)"):
+            print("{filename} file was never encrypted")
+            sys.exit()
+        else:
+            ed_object.decrypt(MD5.new(password).digest(), filename)
+            print(f"Done Decryption for {filename}")
+            os.remove(filename)
+
+    elif choice == "L":
+        print(" \n The files present in the current directory are: ")
         file_list = []
         for content in os.listdir("."):
             file_list.append(content)
-        print file_list
-        
-else:
-        print ""
-        print "Please choose a valid command. Either E or D"
+        print(file_list)
+
+    else:
+        print("\n Please choose a valid command. Either E or D")
         sys.exit()
+
+
+if __name__ == "__main__":
+    choices()
